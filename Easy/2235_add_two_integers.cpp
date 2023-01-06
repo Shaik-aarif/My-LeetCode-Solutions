3bf49836e37c6dc902bf5593b2ea7c8327acbfc2
Arif <0222learning@gmail.com>
2023-01-06 19:57:37 +0530

    Addded solution to problem no 2235

---
+++ Easy/2235_add_two_integers.cpp
@@ -0,0 +1,16 @@
+/*
+Given two integers num1 and num2, return the sum of the two integers.
+*/
+
+/*
+approach : return the addition of two numbers
+*/
+
+class Solution
+{
+public:
+    int sum(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+};
