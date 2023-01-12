42f9d583bba7f7bd15d0c2f9d835d5dddc899c3a
Arif <0222learning@gmail.com>
2023-01-12 20:26:57 +0530

    Addded solution to problem no 69

--- Easy/69_Sqrt(x).cpp
+++ Easy/69_Sqrt(x).cpp
@@ -1,4 +1,24 @@
+/*
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
 
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+*/
+
+/*
+Approach : we have to find the largest y which satisfies the equation fro given x
+            sqrt(x) = y ;
+            sobs
+            x = y*y
+            --> y*y <= x
+
+            y= 0 ;
+            increment y when the condition satisfies
+
+            return y-1;
+
+*/
 class Solution
 {
 public:
