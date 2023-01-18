df73b5423b4b722f0f5e81e2783c4d10a0aa7c2d
Arif <0222learning@gmail.com>
2023-01-18 20:18:14 +0530

    Addded solution to problem no 1108

--- Easy/1108_defanging_an_ip_address.cpp
+++ Easy/1108_defanging_an_ip_address.cpp
@@ -1,3 +1,16 @@
+/*
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+*/
+
+/*
+Approach : 1. traverse through the string
+            2. if u find . add [.] to new string
+            3. else add the char itself to the new string
+            4. return the new string
+
+*/
 
 class Solution
 {
