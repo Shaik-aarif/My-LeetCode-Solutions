6aa27febe2c8aa3e0571020e370da7ba84adfa4f
Arif <0222learning@gmail.com>
2023-01-20 21:39:56 +0530

    Addded solution to problem no 2469

--- Easy/2469_convert_the_temperature.cpp
+++ Easy/2469_convert_the_temperature.cpp
@@ -1,3 +1,24 @@
+/*
+You are given a non-negative floating point number rounded to two decimal places celsius, that denotes the temperature in Celsius.
+
+You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
+
+Return the array ans. Answers within 10-5 of the actual answer will be accepted.
+
+Note that:
+
+Kelvin = Celsius + 273.15
+Fahrenheit = Celsius * 1.80 + 32.00
+
+
+*/
+
+/*
+approach : 1. initialize a vector
+            2. push kelvin value   --> celsius+273.15
+            3. push fahrenheit value  --> celsius*1.80+32.00
+
+*/
 
 class Solution
 {
