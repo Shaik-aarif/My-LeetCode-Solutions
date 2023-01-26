ef447b8310ed742232847d15881be699813f206d
Arif <0222learning@gmail.com>
2023-01-26 11:44:15 +0530

    Addded solution to problem no 2427

--- Easy/2427_Number_of_Common_Factors.cpp
+++ Easy/2427_Number_of_Common_Factors.cpp
@@ -1,3 +1,9 @@
+
+/*
+Given two positive integers a and b, return the number of common factors of a and b.
+
+An integer x is a common factor of a and b if x divides both a and b.
+*/
 class Solution
 {
 public:
