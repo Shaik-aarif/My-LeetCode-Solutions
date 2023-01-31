de91f7429113433d9c0041a648ac1fa47c0e6b6d
Arif <0222learning@gmail.com>
2023-01-31 10:34:14 +0530

    Addded solution to problem no 1431

--- Easy/1431_Kids_With_the_Greatest_Number_of_Candies.cpp
+++ Easy/1431_Kids_With_the_Greatest_Number_of_Candies.cpp
@@ -3,7 +3,7 @@ There are n kids with candies. You are given an integer array candies, where eac
 
 Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
 
-Note that multiple kids can have the greatest number of candies.
+Note that multiple kids can have the greatest no of candies.
 */
 
 class Solution
