3111962018727801c62a9ef701238ef3b6242321
Arif <0222learning@gmail.com>
2023-05-24 11:44:13 +0530

    added solution to problem no 94

---
+++ Easy/94_Binary_Tree_Inorder_Traversal.cpp
@@ -0,0 +1,32 @@
+/*
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        inorder(root , ans);
+        return ans;
+    }
+private:
+    void inorder(TreeNode* root , vector<int> &ans){
+        if(!root)return;
+        inorder(root->left , ans);
+        ans.push_back(root->val);
+        inorder(root->right , ans);
+    }
+};
