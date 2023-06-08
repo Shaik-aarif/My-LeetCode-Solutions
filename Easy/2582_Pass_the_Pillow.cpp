9c5bb338380bb6b184aa4059e045d832e6f2084f
Arif <0222learning@gmail.com>
2023-06-08 16:12:23 +0530

    added solution to problem no 2582

---
+++ Easy/2582_Pass_the_Pillow.cpp
@@ -0,0 +1,29 @@
+/*
+There are n people standing in a line labeled from 1 to n. The first person in the line is holding a pillow initially. Every second, the person holding the pillow passes it to the next person standing in the line. Once the pillow reaches the end of the line, the direction changes, and people continue passing the pillow in the opposite direction.
+
+For example, once the pillow reaches the nth person they pass it to the n - 1th person, then to the n - 2th person and so on.
+Given the two positive integers n and time, return the index of the person holding the pillow after time seconds.
+*/
+
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int ans ;
+        if(n>time){
+            return time+1;
+        }
+        else if (n == time){
+            return n-1;
+        }
+        else{
+        int posi = time % (n-1);
+        if((time/(n-1)) % 2 != 0){
+            ans = n-posi;
+        }else{
+            ans = posi+1;
+        }
+
+        }
+        return ans;
+    }
+};
